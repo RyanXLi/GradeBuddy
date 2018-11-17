0cac12c6375c8c28dcdab9216aaef86efaed3b84
Silas Hsu <hsu.silas@wustl.edu>
2018-11-17 12:41:29 -0600

    Added sidebar functionality and more css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,44 @@
 import React, { Component } from 'react';
+import _ from 'lodash';
 import { Navigation } from './Navigation';
+import uuid from 'uuid/v4';
+
+const COURSE = {
+    id: uuid(),
+    shortName: "CS125 kajshdkjhakdljfhalksdjfhlkajsdhflajshdlkjasdhf",
+    longName: "CS125 Intro to Computer Science",
+    isActive: true,
+    categories: [],
+    assignments: [ // Array of objects with this schema, empty at first
+        {
+            name: "HW1",
+            weight: 0, // If 0, use default weight
+            pointsEarned: 80,
+            pointsPossible: 100,
+            category: "Homeworks",
+            habits: {
+                stat1: 0,
+                stat2: 0,
+            }
+        }
+    ]
+};
+const COURSE2 = _.clone(COURSE);
+COURSE2.id = uuid();
+COURSE2.isActive = false;
+const COURSE3 = _.clone(COURSE);
+COURSE3.id = uuid();
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            courses: [COURSE, COURSE2, COURSE3]
+        };
+    }
+
     render() {
-        return <Navigation />;
+        return <Navigation courses={this.state.courses} />;
     }
 }
 
--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -1,7 +1,89 @@
 import React from 'react';
-import './Navigation.css'
+import PropTypes from 'prop-types';
+import { Collapse } from 'reactstrap';
+import _ from 'lodash';
+import uuid from 'uuid/v4';
+
+import './Navigation.css';
+
+const SIDEBAR_ID = uuid();
 
 export class Navigation extends React.Component {
+    static propTypes = {
+        courses: PropTypes.arrayOf(PropTypes.object)
+    };
+
+    static defaultProps = {
+        courses: []
+    };
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            isAddingCourse: false,
+            selectedCourse: '',
+
+            isSidebarOpen: true,
+            isActiveOpen: true,
+            isInactiveOpen: true,
+        };
+
+        this.toggleBooleanState = this.toggleBooleanState.bind(this);
+    }
+
+    toggleBooleanState(stateName) {
+        this.setState(prevState => {
+            return {[stateName]: !prevState[stateName]}
+        });
+    }
+
+    setSelectedCourse(course) {
+        this.setState({selectedCourse: course.id});
+    }
+
+    renderCourseGroup(courses) {
+        return courses.map(course => {
+            let className;
+            if (course.id === this.state.selectedCourse) {
+                className = 'Navigation-sidebar-course-selected';
+            } else {
+                className = 'Navigation-sidebar-course hoverable';
+            }
+            return <div
+                key={course.id}
+                className={className}
+                onClick={() => this.setSelectedCourse(course)}
+            >
+                {course.shortName}
+            </div>;
+        });
+    }
+
+    renderSidebar() {
+        const [activeCourses, inactiveCourses] = _.partition(this.props.courses, course => course.isActive);
+        return <div id={SIDEBAR_ID} className='Navigation-sidebar'>
+            <div
+                className='Navigation-sidebar-active-heading'
+                onClick={() => this.toggleBooleanState('isActiveOpen')}
+            >
+                Active courses
+            </div>
+            <Collapse isOpen={this.state.isActiveOpen}>
+                {this.renderCourseGroup(activeCourses)}
+            </Collapse>
+
+            <div
+                className='Navigation-sidebar-inactive-heading'
+                onClick={() => this.toggleBooleanState('isInactiveOpen')}
+            >
+                Inactive courses
+            </div>
+            <Collapse isOpen={this.state.isInactiveOpen}>
+                {this.renderCourseGroup(inactiveCourses)}
+            </Collapse>
+        </div>;
+    }
+
     render() {
         const items = [];
         for (let i = 0; i < 100; i++) {
@@ -9,12 +91,16 @@ export class Navigation extends React.Component {
         }
 
         return <div className='Navigation'>
-            <nav className='navbar navbar-expand-lg navbar-light'>
-                <div className='navbar-brand'>Nav</div>
+            <nav className='navbar navbar-expand-lg navbar-light Navigation-navbar'>
+                <div className='navbar-brand'>Grade buddy</div>
             </nav>
             <div className='Navigation-main-container'>
-                <div className='Navigation-sidebar'>
-                    {items}
+                {this.state.isSidebarOpen && this.renderSidebar()}
+                <div
+                    className='Navigation-sidebar-opener hoverable'
+                    onClick={() => this.toggleBooleanState('isSidebarOpen')}
+                >
+                    {this.state.isSidebarOpen ? '‹' : '›'}
                 </div>
                 <div className='Navigation-content-pane'>
                     {items}
