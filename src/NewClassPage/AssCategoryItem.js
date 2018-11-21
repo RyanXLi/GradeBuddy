734d6717a319ab1789de720ce0685c8d3a09f1c1
RyanXLi <xlide@connect.ust.hk>
2018-11-20 20:46:59 -0600

    finished basic framework of NewClassPage

---
+++ src/NewClassPage/AssCategoryItem.js
@@ -0,0 +1,68 @@
+
+import React, { Component } from 'react';
+
+
+class AssCategoryItem extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            checked: false
+        };
+        this.handleCheckedChange = this.handleCheckedChange.bind(this);
+        this.handleWeightChange = this.handleWeightChange.bind(this);
+        this.handleItemClosed = this.handleItemClosed.bind(this);
+    }
+
+    handleItemClosed() {
+        this.props.setItem(this.props.index, {
+            name: this.props.item.name,
+            weight: 0,
+        });
+    }
+
+    handleCheckedChange() {
+        if (this.state.checked) {
+            this.handleItemClosed();
+        }
+        this.setState({
+            checked: !this.state.checked
+        })
+    }
+
+    handleWeightChange(evt) { // TODO: handle invalid input
+        this.props.setItem(this.props.index, {
+            name: this.props.item.name,
+            weight: evt.target.value,
+        });
+    }
+
+
+    render() {
+
+        const weight = this.state.checked
+            ? <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">Weight: </span>
+                </div>
+                <input type="text" className="form-control" onChange={evt => this.handleWeightChange(evt)}   />
+                <div className="input-group-append">
+                    <span className="input-group-text">%</span>
+                </div>
+              </div>
+            : null;
+
+        return (
+            <div>
+                <input type="checkbox"
+                       checked={ this.state.checked }
+                       onChange={ this.handleCheckedChange }/>
+                <span>{this.props.item.name}</span>
+
+                {weight}
+            </div>
+        );
+    }
+}
+
+export default AssCategoryItem;
--- src/NewClassPage/NewClassPage.js
+++ src/NewClassPage/NewClassPage.js
@@ -3,12 +3,79 @@
  */
 
 import React, { Component } from 'react';
+import AssCategoryItem from "./AssCategoryItem";
 
 
 class NewClassPage extends Component {
 
+    constructor() {
+        super();
+        this.state = {
+            courseTitle: '',
+            courseSubject: '',
+            courseCode: '',
+            // shortName: null, // string
+            // longName: null, // string
+            isActive: true, // boolean
+            categories: [
+                {name: 'Homework', weight: 0.0},
+                {name: 'Project', weight: 0.0},
+                {name: 'Midterm', weight: 0.0},
+                {name: 'Final', weight: 0.0},
+                {name: 'Quizzes', weight: 0.0},
+            ],
+            // Array of objects with this schema, empty at first
+            //     {
+            //         name: string,
+            //         weight: number
+            //     }
+            assignments: [ ],
+        };
+        this.setItem = this.setItem.bind(this);
+    }
+
+    updateCourseTitle(evt) {
+        this.setState({
+            courseTitle: evt.target.value
+        });
+        // console.log(this.state.courseTitle);
+    }
+
+    updateCourseSubject(evt) {
+        this.setState({
+            courseSubject: evt.target.value
+        });
+        // console.log(this.state.courseSubject);
+    }
+
+    updateCourseCode(evt) {
+        this.setState({
+            courseCode: evt.target.value
+        });
+        // console.log(this.state.courseCode);
+    }
+
+
+    setItem(i, obj) {
+        const copy = this.state.categories.slice();
+        copy[i] = obj;
+        this.setState({
+            categories: copy
+        });
+    }
+
+
     render() {
+
+        let assCategoryItems = [];
+        for (let i = 0; i < this.state.categories.length; i++) {
+            assCategoryItems.push(
+                <AssCategoryItem key={i.toString()} index={i} item={this.state.categories[i]} setItem={this.setItem}/>
+            );
+        }
+
         return (
+            <div>
             <div className="NewClassPage">
                 <div className="title">Add new class</div>
                 <div className="content">
@@ -23,25 +90,24 @@ class NewClassPage extends Component {
                                     </h5>
                                 </div>
 
-                                <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                                <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" >
                                     <div className="card-body">
 
 
 
                                         <form>
                                             <div className="form-group">
-                                                <label for="exampleInputEmail1">Email address</label>
-                                                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                                <label htmlFor="courseTitle">Course Title</label>
+                                                <input className="form-control" id="courseTitle" value={this.state.courseTitle} onChange={evt => this.updateCourseTitle(evt)} placeholder="System Programming"/>
                                             </div>
                                             <div className="form-group">
-                                                <label for="exampleInputPassword1">Password</label>
-                                                <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                                                <label htmlFor="courseSubject">Course Subject</label>
+                                                <input className="form-control" id="courseSubject" value={this.state.courseSubject} onChange={evt => this.updateCourseSubject(evt)} placeholder="CS" />
                                             </div>
-                                            <div className="form-group form-check">
-                                                <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                                                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                                            <div className="form-group">
+                                                <label htmlFor="courseCode">Course Code</label>
+                                                <input className="form-control" id="courseCode" value={this.state.courseCode} onChange={evt => this.updateCourseCode(evt)} placeholder="241" />
                                             </div>
-                                            <button type="submit" className="btn btn-primary">Submit</button>
                                         </form>
 
 
@@ -59,24 +125,15 @@ class NewClassPage extends Component {
                                         </button>
                                     </h5>
                                 </div>
-                                <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                                <div id="collapseTwo" className="collapse show" aria-labelledby="headingTwo" >
                                     <div className="card-body">
 
 
-                                        <div className="input-group mb-3">
-                                            <div className="input-group-prepend">
-                                                <div className="input-group-text">
-                                                    <input type="checkbox" aria-label="Checkbox for following text input" />
-                                                </div>
-                                            </div>
-                                            <input type="text" className="form-control" aria-label="Text input with checkbox" />
-                                        </div>
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
+
+                                        {assCategoryItems}
+                                        <button type="submit" className="btn btn-primary save-button">Add</button>
+
+
                                     </div>
                                 </div>
                             </div>
@@ -89,8 +146,11 @@ class NewClassPage extends Component {
                     </div>
                 </div>
             </div>
+            </div>
     );
     }
     }
 
+    // TODO: silas please add export where appropriate, note that
+    // TODO: `this.state` is close to the object described in the schema
     export default NewClassPage;
