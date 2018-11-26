56d0099756269fd8650b9eb24afc8cbadacda262
Srilu <srilucode@users.noreply.github.com>
2018-11-26 11:38:12 -0600

    Updated CourseHomePage.jsx - please add!

--- src/CourseHomePage/CourseHomePage.jsx
+++ src/CourseHomePage/CourseHomePage.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import './CourseHomePage.css';
 
+/**
+ * Content pane displaying the assignment details of course selected.
+ * 
+ * @author Srilakshmi Prasad
+ */
+
+export function CourseHomePage() {
+    return <div className='CourseHomePage'>
+        <span className='CourseHomePage-inner'>
+            Now displaying INSERT COURSE NAME
+        </span>
+    </div>;
+}
