d7e4e3a67ef4db3d3fe0036bd45a11676d12f906
Srilu <srilucode@users.noreply.github.com>
2018-11-27 16:07:00 -0600

    Update CourseHomePage.jsx

--- src/CourseHomePage/CourseHomePage.jsx
+++ src/CourseHomePage/CourseHomePage.jsx
@@ -1,17 +1,102 @@
 import React from 'react';
 import { CollapseWithHeading } from '../CollapseWithHeader';
-
 import './CourseHomePage.css';
+import { HabitPage } from '../HabitPage/HabitPage';
 
 /**
  * Content pane displaying the assignment details of course selected.
  * 
  * @author Srilakshmi Prasad
  */
-export function CourseHomePage(props) {
-    return <div className='CourseHomePage'>
-        <CollapseWithHeading headingText='asdf'>
-            {props.selectedCourse.longName}
-        </CollapseWithHeading>
-    </div>;
+export class CourseHomePage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			habitBeingEdited: ''
+		};
+	}
+
+	render() {
+		if (this.state.habitBeingEdited) {
+			return <HabitPage />;
+		}
+
+		return <div className='CourseHomePage' style={{flexDirection: 'column', alignItems: 'flex-start'}}>
+	    	<div style={{marginBottom:50}}>
+	    		{this.props.selectedCourse.shortName}
+	    	</div>
+	        <CollapseWithHeading headingText='Homework'>
+	            <table class="table table-hover">
+				  <tbody>
+				    <tr>
+				      <th scope="row">Homework 1 (33%)</th>
+				      <td>80/100</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'foo'})}>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Homework 2 (33%)</th>
+				      <td>70/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Homework 3 (33%)</th>
+				      <td>100/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				  </tbody>
+				</table>
+	        </CollapseWithHeading>
+	        <CollapseWithHeading headingText='Exam'>
+	            <table class="table table-hover">
+				  <tbody>
+				    <tr>
+				      <th scope="row">Exam 1 (33%)</th>
+				      <td>80/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Exam 2 (33%)</th>
+				      <td>70/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Exam 3 (33%)</th>
+				      <td>100/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				  </tbody>
+				</table>
+	        </CollapseWithHeading>
+	        <CollapseWithHeading headingText='Quiz'>
+	            <table class="table table-hover">
+				  <tbody>
+				    <tr>
+				      <th scope="row">Quiz 1 (33%)</th>
+				      <td>80/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Quiz 2 (33%)</th>
+				      <td>70/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				    <tr>
+				      <th scope="row">Quiz 3 (33%)</th>
+				      <td>100/100</td>
+				      <td>Edit Habit</td>
+				      <td>Trash</td>
+				    </tr>
+				  </tbody>
+				</table>
+	        </CollapseWithHeading>
+    	</div>;
+	}
 }
