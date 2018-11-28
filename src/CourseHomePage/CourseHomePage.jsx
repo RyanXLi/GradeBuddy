f9442cde493363723d7a9a9695f8e1aa3f82ea67
Alex <alex.blacketor@gmail.com>
2018-11-28 01:10:41 -0600

    added onclick actions for habit buttons on course home page

--- src/CourseHomePage/CourseHomePage.jsx
+++ src/CourseHomePage/CourseHomePage.jsx
@@ -32,19 +32,19 @@ export class CourseHomePage extends React.Component {
 				    <tr>
 				      <th scope="row">Homework 1 (33%)</th>
 				      <td>80/100</td>
-				      <td onClick={() => this.setState({habitBeingEdited: 'foo'})}>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Homework 1'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Homework 2 (33%)</th>
 				      <td>70/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Homework 2'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Homework 3 (33%)</th>
 				      <td>100/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Homework 3'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				  </tbody>
@@ -56,19 +56,19 @@ export class CourseHomePage extends React.Component {
 				    <tr>
 				      <th scope="row">Exam 1 (33%)</th>
 				      <td>80/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Exam 1'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Exam 2 (33%)</th>
 				      <td>70/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Exam 2'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Exam 3 (33%)</th>
 				      <td>100/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Exam 3'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				  </tbody>
@@ -80,19 +80,19 @@ export class CourseHomePage extends React.Component {
 				    <tr>
 				      <th scope="row">Quiz 1 (33%)</th>
 				      <td>80/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Quiz 1'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Quiz 2 (33%)</th>
 				      <td>70/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Quiz 2'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				    <tr>
 				      <th scope="row">Quiz 3 (33%)</th>
 				      <td>100/100</td>
-				      <td>Edit Habit</td>
+				      <td onClick={() => this.setState({habitBeingEdited: 'Quiz 3'})}>Edit Habit</td>
 				      <td>Trash</td>
 				    </tr>
 				  </tbody>
