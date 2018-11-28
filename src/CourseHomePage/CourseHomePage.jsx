6e57ad65abc0259f6c1ef39316fbdfbc7782dfde
Srilu <srilucode@users.noreply.github.com>
2018-11-28 17:36:39 -0600

    Edited header style

--- src/CourseHomePage/CourseHomePage.jsx
+++ src/CourseHomePage/CourseHomePage.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import _ from 'lodash';
 import { CollapseWithHeading } from '../CollapseWithHeader';
 import { HabitPage } from '../HabitPage/HabitPage';
 import './CourseHomePage.css';
@@ -11,71 +9,95 @@ import './CourseHomePage.css';
  * @author Srilakshmi Prasad
  */
 export class CourseHomePage extends React.Component {
-    static propTypes = {
-        selectedCourse: PropTypes.object.isRequired,
-        onCourseEdited: PropTypes.func
-    };
-
     constructor(props) {
         super(props);
         this.state = {
-            habitBeingEdited: '' // ID of the assignment whose habits are being edited
+            habitBeingEdited: ''
         };
     }
 
-    setHabitBeingEdited(assignment) {
-        this.setState({habitBeingEdited: assignment.id});
-    }
-
-    renderCategoryTable(category, assignments=[]) {
-        const assignmentRows = assignments.map(assignment => (
-            <tr key={assignment.id}>
-                <td>{assignment.name}</td>
-                <td>{assignment.pointsEarned}/{assignment.pointsPossible}</td>
-                <td>
-                    <span className='btn-link' onClick={() => this.setHabitBeingEdited(assignment)}>
-                        Add habits
-                    </span>
-                </td>
-                <td><i className="fa fa-trash"/></td>
-            </tr>
-        ));
-
-        return <CollapseWithHeading key={category.name} headingText={category.name}>
-            <table className="table table-hover">
-                <thead>
-                    <tr>
-                        <th>Assignment</th><th>Grade</th><th>Habits</th><th>Delete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {assignmentRows}
-                    <tr><td colSpan={4}><i className="fa fa-plus"/></td></tr>
-                </tbody>
-            </table>
-        </CollapseWithHeading>
-    }
-
     render() {
-        const course = this.props.selectedCourse;
-        const habitBeingEdited = this.state.habitBeingEdited
-        if (habitBeingEdited) {
-            const assignment = course.assignments.find(assignment => assignment.id === habitBeingEdited);
-            return <HabitPage habits={assignment.habits} onDataSaved={_.noop} onCancel={_.noop} />;
+        if (this.state.habitBeingEdited) {
+            return <HabitPage />;
         }
 
-        const categoryNames = new Set(course.categories.map(category => category.name));
-        const assignmentsForCategory = _.groupBy(course.assignments,
-            assignment => categoryNames.has(assignment.category) && assignment.category);
-
-        return <div className='CourseHomePage'>
-            <div style={{marginBottom:50}}>
+        //EDIT HABIT should be clickable and lead to HabitPage, passing correct params
+        return <div className='CourseHomePage' style={{flexDirection: 'column', alignItems: 'flex-start'}}>
+            <div style={{marginBottom:50, fontWeight: 'bold', color: 'black', fontStyle: 'normal', fontSize: 30}}>
                 {this.props.selectedCourse.shortName}
             </div>
-            {course.categories.map(
-                category => this.renderCategoryTable(category, assignmentsForCategory[category.name])
-            )}
-            {this.renderCategoryTable({name: 'Uncategorized'}, assignmentsForCategory[false])}
+            <CollapseWithHeading headingText='Homework'>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Homework 1 (33%)</th>
+                            <td>80/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Homework 1'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Homework 2 (33%)</th>
+                            <td>70/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Homework 2'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Homework 3 (33%)</th>
+                            <td>100/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Homework 3'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </CollapseWithHeading>
+            <CollapseWithHeading headingText='Exam'>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Exam 1 (33%)</th>
+                            <td>80/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Exam 1'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Exam 2 (33%)</th>
+                            <td>70/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Exam 2'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Exam 3 (33%)</th>
+                            <td>100/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Exam 3'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </CollapseWithHeading>
+            <CollapseWithHeading headingText='Quiz'>
+                <table class="table table-hover">
+                    <tbody>
+                        <tr>
+                            <th scope="row">Quiz 1 (33%)</th>
+                            <td>80/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Quiz 1'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Quiz 2 (33%)</th>
+                            <td>70/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Quiz 2'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Quiz 3 (33%)</th>
+                            <td>100/100</td>
+                            <td onClick={() => this.setState({habitBeingEdited: 'Quiz 3'})}>Edit Habit</td>
+                            <td>Trash</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </CollapseWithHeading>
         </div>;
     }
 }
