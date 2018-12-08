c443a59566fa79d5ac0a8dc52cf4d908234d87ec
Silas Hsu <hsu.silas@wustl.edu>
2018-12-07 23:47:28 -0600

    Updates

---
+++ precache-manifest.c1c3bce5c5c5bea32f957abd775a9696.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "597e6976b26edf96c19f07e7debf5144",
+    "url": "/potatobase/static/media/welcome_image_with_text.597e6976.jpg"
+  },
+  {
+    "revision": "67537171e9eabba6a922e1fd08066dc7",
+    "url": "/potatobase/static/media/single_bulb_logo.67537171.png"
+  },
+  {
+    "revision": "33a14ce74c4ecbacef00",
+    "url": "/potatobase/static/js/runtime~main.33a14ce7.js"
+  },
+  {
+    "revision": "89b09c17649cc3771e45",
+    "url": "/potatobase/static/js/main.89b09c17.chunk.js"
+  },
+  {
+    "revision": "220a1efb93e7f362b1d7",
+    "url": "/potatobase/static/js/1.220a1efb.chunk.js"
+  },
+  {
+    "revision": "89b09c17649cc3771e45",
+    "url": "/potatobase/static/css/main.77bed325.chunk.css"
+  },
+  {
+    "revision": "e7ec133e1f1c900fcbb7e0f21040532d",
+    "url": "/potatobase/index.html"
+  }
+];
